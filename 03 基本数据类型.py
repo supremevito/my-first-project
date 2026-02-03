50abd45bedfd97fb69453944c1416272a34e6257
supermanvito <supermanvito@outlook.com>
2026-02-03 23:08:57 +0800

    again

---
+++ 03 基本数据类型.py
@@ -0,0 +1,26 @@
+#一、字符串
+name ="v"
+gender = 'male'
+introduce  = """
+v老师今天很好
+v
+vs
+"""
+
+print(name)
+print(gender)
+print(introduce)
+
+#二、数字类型 整型和浮点型
+age = 18
+height = 1.95
+#浮点型
+
+#三、布尔类型 True False
+is_married = False
+# is_married = "否"
+if is_married:
+    print("不能找啰")
+else:
+    print("找一个")
+
