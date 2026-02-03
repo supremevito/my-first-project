f06de8d46c83757712dce8000e369cee73006db4
supermanvito <supermanvito@outlook.com>
2026-02-03 17:36:23 +0800

    again

---
+++ 02 变量.py
@@ -0,0 +1,44 @@
+# 一、基本数据类型
+# 字符串 "" ''
+# 数字 1 2 3
+# 布尔类型 Ture False
+
+# 二、高级数据类型
+# 列表 []
+# 字典 {}
+
+'奥说'
+
+# print(1 + 2)
+# print(1 - 2)
+# print(1 * 2)
+# print(1 / 2)
+
+#Ctrl + D
+#Ctrl + /
+
+# a=1
+# b=2
+#
+# # 用变量代指常用的值
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+
+# print(100)
+# x =100
+# print(x)
+
+#用不用变量 开拓空间 取决于你后面还用不用 这个100
+
+#变量怎么变？
+level =1
+print(level)
+
+#省略：闯关成功，登记加一
+level = 1 + 1
+print(level)
+#python跟c/c++不一样 python变量重新赋值是开一个新创建的数据空间 然后重新指向这个新的空间
+# 把原来空间垃圾回收了 python所有的空间都不能改
