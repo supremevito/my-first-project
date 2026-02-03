8af7439b14f4fcec47bc5013ba77e355b9a81568
supermanvito <supermanvito@outlook.com>
2026-02-03 23:18:21 +0800

    again

---
+++ 04 运算符.py
@@ -0,0 +1,22 @@
+# 语句：由表达式构成 1+2 和 x =3
+# 表达式是由数据和运算符构成
+# x = 1+2
+
+# 一、科学运算
+print(1 + 3)
+print(1 - 3)
+print(1 * 3)
+print(1 / 3)
+# 二、比较运算符
+print(2 > 1)  # True
+print(2 == 1)  # False
+
+# 三、赋值运算符 = +=
+a = 1
+level = 8
+print("闯关成功等级自动+1")
+level = level + 1
+level += 1  # python 只能这样 level+1 不能 ++
+blood = 80
+print("受到攻击，生命减10")
+blood -= 10
